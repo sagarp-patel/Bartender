0145efefe0370d43f79442033ce175456b8c5cc9
Sagar Patel <sagar10patel@gmail.com>
2019-06-22 18:48:06 -0700

    Added Changes to Layout

--- Android_App/MySmartBartender/app/src/main/java/com/sagarpatel/mysmartbartender/MainActivity.kt
+++ Android_App/MySmartBartender/app/src/main/java/com/sagarpatel/mysmartbartender/MainActivity.kt
@@ -2,20 +2,34 @@ package com.sagarpatel.mysmartbartender
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.view.View
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.options_Button)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
     }
 
 
+
     // Make Drink Function. Sends a signal to the RPi to make a drink selected from the spinner object
-    fun make_Drink(view: View){
+    fun pour_Drink(view: View) {
         //Process
         //1st get the drink selected from spinner,
         //2nd send the signal corresponding to that to RPi
+        Snackbar.make(view, "Function is still in development", Snackbar.LENGTH_LONG)
+            .setAction("Action", null)
+            .show()
+
     }
 
 }
