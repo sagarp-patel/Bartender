787b7a5c8a4eabe0366a7ff78c005e15e2b33c9d
Sagar Patel <sagar10patel@gmail.com>
2019-06-26 14:14:22 -0700

    Added add drink Fragment

--- Android_App/MySmartBartender/app/src/main/java/com/sagarpatel/mysmartbartender/MainActivity.kt
+++ Android_App/MySmartBartender/app/src/main/java/com/sagarpatel/mysmartbartender/MainActivity.kt
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
+import android.support.v4.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +18,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var pump_config_fab:View
     lateinit var add_drink_fab:View
     lateinit var info_about_fab:View
+    lateinit var drink_menu:Spinner
+    lateinit var menu_adapter:ArrayAdapter<CharSequence>
+    lateinit var menu_array_list:ArrayList<CharSequence>
     var isMenuOpen:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +30,12 @@ class MainActivity : AppCompatActivity() {
         fab = findViewById(R.id.options_Button)
         add_drink_fab = findViewById(R.id.add_drink_button)
         info_about_fab = findViewById(R.id.info_button)
+        drink_menu = findViewById(R.id.drink_menu_spinner)
+        menu_array_list = ArrayList<CharSequence>()
+        menu_array_list.add("No Drinks")
+        menu_adapter = ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_dropdown_item,menu_array_list)
+        drink_menu.adapter = menu_adapter
+
         //openMenu()
         closeMenu()
     }
@@ -31,7 +48,6 @@ class MainActivity : AppCompatActivity() {
             openMenu()
             isMenuOpen = true
         }
-
     }
 
     fun openMenu(){
@@ -54,6 +70,26 @@ class MainActivity : AppCompatActivity() {
         Snackbar.make(view, "Function is still in development", Snackbar.LENGTH_LONG)
             .setAction("Action", null)
             .show()
+    }
+
+    fun changeFragment(view:View){
+        var fragment:Fragment
+
+        if(view == findViewById(R.id.add_drink_button)){
+            fragment = Add_Drink()
+            var fragmentManager:FragmentManager = supportFragmentManager
+            var fragmentTransaction:FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container,fragment)
+        }
+    }
+
+    fun add_Drink(view:View){
+        //changeFragment(view)
+        var fragment:Fragment
+        fragment = Add_Drink()
+        var fragmentManager:FragmentManager = getSupportFragmentManager()//supportFragmentManager
+        var fragmentTransaction:FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container,fragment)
     }
 
 }
